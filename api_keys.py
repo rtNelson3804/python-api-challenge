0cc82b4edac45a98551cf6475e2298d59d7a3527
rtNelson3804 <randall.nelson28@gmail.com>
2025-04-22 02:28:39 -0500

    123

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "2d1dcb130183712dff4bb57d0832fd49"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "082203bed87c474a8ac045e3f176ea9b"
